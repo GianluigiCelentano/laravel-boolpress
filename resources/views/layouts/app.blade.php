b539496445173af50fa8fbda1ff8d6daa078774a
GianluigiCelentano <cgianluigi96@gmail.com>
2021-09-16 17:39:50 +0200

    Home and Add created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/post') }}">
                     <!-- {{ config('app.name', 'Laravel') }} -->
-                    Home
+                    Home 
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>

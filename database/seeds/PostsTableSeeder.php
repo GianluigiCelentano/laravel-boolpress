4f2c7ed2cc3f699e9f4d7e87a373fab88a61baca
GianluigiCelentano <cgianluigi96@gmail.com>
2021-09-22 17:02:33 +0200

    push x hotwell

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -13,11 +13,20 @@ class PostsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         for ($i=0; $i < 50; $i++) { 
+
+            $postDetail=new PostDetail();
+            $postDetail->thematic = $faker->words(1, true);
+            $postDetail->type = $faker->words(1, true);
+            $postDetail->save();
+
             $postObject = new Post();
             $postObject->title = $faker -> sentence(3);
             $postObject->postText = $faker -> paragraph(2);
             $postObject->author = $faker -> name();
             $postObject->cover = $faker -> imageUrl(640, 480, 'animals', true);
+
+            $postObject->post_detail_id = $postDetail->id;
+
             $postObject-> save();
         }
     }
